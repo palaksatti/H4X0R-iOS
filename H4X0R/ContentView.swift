adfae1f1d0236e644a10fd9dc7e01db0b380652b
Palak <palak.satti@gmail.com>
2024-02-06 10:53:23 +0530

    Initial Commit

---
+++ H4X0R/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by Palak Satti on 06/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Palak Satti on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
